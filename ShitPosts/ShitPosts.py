6494f26d423625797da7f580ce05d55d760ad12a
TheLazyHatGuy <cscholes982@gmail.com>
2017-08-16 01:26:00 +0100

    Fixed colour
    I didn't use the new colour thing properly or something. I messed up somewhere along the line

--- ShitPosts/ShitPosts.py
+++ ShitPosts/ShitPosts.py
@@ -29,10 +29,10 @@ class ShitPosts():
         print(part)
         if part == 1:
             Bee1 = "According to all known laws of aviation,\n there is no way a bee should be able to fly.\n Its wings are too small to get its fat little body off the ground.\n The bee, of course, flies anyway because bees don't care what humans think is impossible.\nYellow, black. Yellow, black.\nYellow, black. Yellow, black.\nOoh, black and yellow!\nLet's shake it up a little.\nBarry! Breakfast is ready!\nOoming!\nHang on a second.\n"
-            BeeScript = discord.Embed(description=Bee1, colour=discord.Colour(value=color), title="Bee Movie Pt.1")
+            BeeScript = discord.Embed(description=Bee1, colour=discord.Colour(value=randomcolor()), title="Bee Movie Pt.1")
         elif part == 2:
             Bee2 = "- Hey, Adam.\n- Hey, Barry.\n- Is that fuzz gel?\n- A little. Special day, graduation.\nNever thought I'd make it.\nThree days grade school, three days high school.\nThose were awkward.\nThree days college. I'm glad I took a day and hitchhiked around the hive.\nYou did come back different.\n- Hi, Barry.\n- Artie, growing a mustache? Looks good.\n- Hear about Frankie?\n- Yeah.\n- You going to the funeral?\n- No, I'm not going.\nEverybody knows, sting someone, you die.\nDon't waste it on a squirrel.\nSuch a hothead.\nI guess he could have just gotten out of the way.\nI love this incorporating an amusement park into our day.\nThat's why we don't need vacations.\nBoy, quite a bit of pomp... under the circumstances.\n- Well, Adam, today we are men.\n- We are!\n- Bee-men.\n- Amen!\nHallelujah!\nStudents, faculty, distinguished bees, please welcome Dean Buzzwell.\nWelcome, New Hive Oity graduating class of...\n...9:15.\nThat concludes our ceremonies.\nAnd begins your career at Honex Industries!\nWill we pick our job today?\nI heard it's just orientation.\nHeads up! Here we go.\nKeep your hands and antennas inside the tram at all times.\n"
-            BeeScript = discord.Embed(description=Bee2, colour=discord.Colour(value=color), title="Bee Movie Pt.2")
+            BeeScript = discord.Embed(description=Bee2, colour=discord.Colour(value=randomcolor()), title="Bee Movie Pt.2")
         else:
             BeeScript = discord.Embed(description="Part doesn't exist", colour=discord.Colour(randomcolor()),
                                       title="Ha lul")
